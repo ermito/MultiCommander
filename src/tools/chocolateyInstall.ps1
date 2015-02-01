34310044d71c4c7aa9fca684856aada9adef0dbf
ermito <dmitri-diavo@mail.ru>
2015-02-01 23:11:03 +0300

    ver 5.0 add autohotkey

--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -1,6 +1,22 @@
 ﻿$packageName = 'MultiCommander'
 $installerType = 'EXE'
-$url = 'http://multicommander.com/files/updates/MultiCommander_win32_(4.6.2.1804).exe'
-$url64 = 'http://multicommander.com/files/updates/MultiCommander_x64_(4.6.2.1804).exe'
+$url = 'http://multicommander.com/files/updates/MultiCommander_win32_(5.0.0.1888).exe'
+$url64 = 'http://multicommander.com/files/updates/MultiCommander_x64_(5.0.0.1888).exe'
 $validExitCodes = @(0)
-Install-ChocolateyPackage "$packageName" "$installerType" "$url" "$url64"  -validExitCodes $validExitCodes
+$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+$ahkFile = "$scriptPath\multicommander.ahk"
+$packageName = 'multicommander'
+$fileFullPath = "$filePath\${packageName}Install.exe"
+
+try {
+	Get-ChocolateyWebFile $packageName $fileFullPath $url $url64
+	Start-Process 'AutoHotkey' $ahkFile
+	Start-Process $fileFullPath
+	Wait-Process -Name "${packageName}Install"
+	Remove-Item $fileFullPath
+	Write-ChocolateySuccess $packageName 
+} catch { 
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw
+}
+
---
+++ src/tools/multicommander.ahk
@@ -0,0 +1,32 @@
+﻿#NoEnv
+SendMode Input
+SetWorkingDir %A_ScriptDir%
+
+SetTitleMatchMode, 2
+
+WinWait, MultiCommander ahk_class #32770, , 60
+Sleep, 200
+WinActivate
+IfWinActive
+	ControlClick, Button2
+	ControlClick, Button2
+	Sleep, 200
+	Control, Check, ,Button4
+	ControlClick, Button2
+	ControlClick, Button2
+	ControlClick, Button2
+	ControlClick, Button2
+	ControlClick, Button2
+;wait finish 30 sec
+Loop, 30
+{
+	ControlGetText, str, Button2
+	IfInString, str, Finish
+	{
+		Sleep, 200
+		ControlClick, Button2
+		break
+	}
+	else
+		Sleep, 1000
+}
