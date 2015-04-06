05ae150a9849295b3272fad677a22c42474d4fce
ermito <dmitri-diavo@mail.ru>
2015-04-07 00:33:41 +0300

    upd 5.1

--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'MultiCommander'
 $installerType = 'EXE'
-$url = 'http://multicommander.com/files/updates/MultiCommander_win32_(5.0.0.1888).exe'
-$url64 = 'http://multicommander.com/files/updates/MultiCommander_x64_(5.0.0.1888).exe'
+$url = 'http://multicommander.com/files/updates/MultiCommander_win32_(5.1.0.1922).exe'
+$url64 = 'http://multicommander.com/files/updates/MultiCommander_x64_(5.1.0.1922).exe'
 $validExitCodes = @(0)
 $silentArgs = '/S'
 
