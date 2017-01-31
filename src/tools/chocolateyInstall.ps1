9572e373504097217681700a797414932c23c5a6
ermito <dmitri-diavo@mail.ru>
2017-01-31 17:45:04 +0300

    upd to 6.9.1

--- src/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$packageName = 'MultiCommander'
-$installerType = 'EXE'
-$url = 'http://multicommander.com/files/updates/MultiCommander_win32_(5.5.0.1975).exe'
-$url64 = 'http://multicommander.com/files/updates/MultiCommander_x64_(5.5.0.1975).exe'
-$validExitCodes = @(0)
-$silentArgs = '/S'
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
